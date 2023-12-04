6c298f2fce45a6a51563c1023dd46f65c904efc0
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2023-12-04 11:03:10 +0100

    Add project files.

---
+++ Horse.cs
@@ -0,0 +1,53 @@
+﻿namespace ServicesExercises
+{
+    internal class Horse
+    {
+        public Horse(int position)
+        {
+            _position = position;
+        }
+
+        private static string[] HorseRepresentation { get; } = { "*", "+", "-", "$", "@" };
+        private static object _l = new();
+        private int X { get; set; }
+        private int Xx { get; set; }
+        private readonly Random _random = new();
+        private int _position;
+        private static bool _isRaceFinished = false;
+
+        public void Run()
+        {
+            var sleepTime = _random.Next(100, 200);
+            while (X < 50 && !_isRaceFinished)
+            {
+                lock (_l)
+                {
+                    Console.SetCursorPosition(Xx, _position + 20);
+                    Console.WriteLine(Thread.CurrentThread.Name);
+                    Console.SetCursorPosition(X, _position + 5);
+                    Console.WriteLine(HorseRepresentation[_position]);
+                }
+                this.X += _random.Next(1, 5);
+                this.Xx += Thread.CurrentThread.Name.Length + 1;
+                Thread.Sleep(sleepTime);
+            }
+
+            lock (_l)
+            {
+                _isRaceFinished = true;
+            }
+
+            lock (_l)
+            {
+                Console.SetCursorPosition(Xx, _position + 20);
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.SetCursorPosition(X, _position + 5);
+                Console.WriteLine(HorseRepresentation[_position]);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ HorseRaceExercise5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ServicesExercises
+{
+    internal class HorseRaceExercise5
+    {
+        
+        private const int UPPER = 5;
+        public static Thread[] threads = new Thread[UPPER];
+        private static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hihihi");
+            HorseRace();
+            Console.ReadKey();
+        }
+
+        public static void HorseRace()
+        {
+            var sleepTime = _random.Next(100, 200);
+            for (int i = 0; i < UPPER; i++)
+            {
+                Horse horse = new(i);
+                var thread = new Thread(horse.Run);
+                thread.Name = $"{i}";
+                threads[i] = thread;
+                
+                
+            }
+
+            for (int i = 0; i < UPPER; i++)
+            {
+                threads[i].Start();   
+                Thread.Sleep(sleepTime);
+            }
+        }
+
+        
+    }
+}
